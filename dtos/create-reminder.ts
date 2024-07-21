0fa8837a6b7c5cf4abae7425919d3c0abfb48cf2
tuposilwe <rudigermkondya@gmail.com>
2024-07-21 13:34:52 +0300

    Parsing Request Bodies

---
+++ dtos/create-reminder.ts
@@ -0,0 +1,4 @@
+export default interface CreateReminderDto {
+    title: string
+  }
+  
--- routers/reminders.ts
+++ routers/reminders.ts
@@ -1,9 +1,16 @@
 import { Router } from "express";
+import CreateReminderDto from "../dtos/create-reminder";
 
 const router = Router();
 
 router.get('/', (req,res) => {
     res.send('List of reminders');
+});
+
+
+router.post('/', (req,res) => {
+   const {title} = req.body as CreateReminderDto;
+   res.json(title);
 });
 
 export default router;
